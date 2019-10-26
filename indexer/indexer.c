a452f65b39a94eb474d4168260a7e436eb3a20e2
stjepanvrbic <stjepan.vrbic@gmail.com>
2019-10-26 18:06:03 -0400

    indexer

---
+++ indexer/indexer.c
@@ -0,0 +1,18 @@
+/* indexer.c ---
+ * 
+ * 
+ * Author: Stjepan Vrbic, Allen Ma
+ * 
+ * HTLM indexer
+ * 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void){
+    
+    
+
+    return (EXIT_SUCCESS);
+}
